b05e1f2b3a1c5683a5d9cc3b3844bf78a5019e4f
Rishit Tiwari <128586763+rixitgithub@users.noreply.github.com>
2024-01-23 21:34:06 +0530

    Stack Widget

---
+++ 6)Stack Widget.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  List names = ["Rishit", "Billu", "Kaliya"];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            //big box
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.deepPurple,
+            ),
+            //medium box
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.deepPurple[400],
+            ),
+            //small box
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.deepPurple[200],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
