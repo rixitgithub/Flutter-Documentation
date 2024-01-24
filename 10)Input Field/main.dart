6508bfa7895e7d68528f446c026fcf5f9c080c58
Rishit Tiwari <128586763+rixitgithub@users.noreply.github.com>
2024-01-24 11:49:41 +0530

    Main

---
+++ 10)Input Field/main.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'pages/todo_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ToDoPage(),
+    );
+  }
+}
